f0511a5b1b0fc1a68c5cc4c4035cb2428fbbe920
Sumathi D <sumathid403@gmail.com>
2025-04-26 13:20:14 +0530

    added link for projects

--- src/components/project.js
+++ src/components/project.js
@@ -8,18 +8,21 @@ const projects = [
   {
     title: "Portfolio Website",
     image: PF,
+    link:"https://dailyplanner-sumathi.vercel.app/",
     description:
       "A responsive personal portfolio website built using React.js and CSS. The site showcases my work, skills, and contact information in a clean and organized layout. It features smooth navigation, modern styling, and reusable React components for scalability and maintainability.",
   },
   {
     title: "E-Commerce App",
     image: j1,
+    link:"https://dailyplanner-sumathi.vercel.app/",
     description:
       "A full-stack e-commerce application using MERN stack with features like user authentication, shopping cart, and Stripe integration. Explore a wide range of products, manage your cart, and experience a seamless checkout process.",
   },
   {
     title: "Daily Planner",
     image: dp,
+    link:"https://dailyplanner-sumathi.vercel.app/",
     description:
       "A simple and efficient task management application built using React.js and CSS. This app allows users to add, edit and delete. It focuses on clean design, user-friendly interactions, and component-based architecture.",
   },
@@ -32,10 +35,16 @@ const Project = () => {
         <h2 className="project-heading">My Projects</h2>
         <div className="project-grid">
           {projects.map((project, index) => (
+            <a 
+            href={project.link}
+            rel="noopener noreferrer"
+            key={index}
+            className="project-card"
+            target="_blank">
             <div
               className="project-card"
               key={index}
-              style={{ "--delay": `${index * 0.15}s` }}
+              // style={{ "--delay": `${index * 0.15}s` }}
             >
               <div className="image-container">
                 <img
@@ -50,7 +59,9 @@ const Project = () => {
                   </div>
                 </div>
               </div>
+             
             </div>
+            </a>
           ))}
         </div>
       </section>
